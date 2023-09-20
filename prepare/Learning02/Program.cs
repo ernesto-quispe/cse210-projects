68899caec2a906b14661ee0eeab42dee537f8cc6
ernesto.quispe <ernesto.cajan@gmail.com>
2023-09-20 13:46:40 -0300

    Abstraction Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "2014";
+        job1._endYear = "2017";
+
+        //Console.WriteLine($"{job1._company} needs a {job1._jobTitle} in {job1._address}");
+        //job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._jobTitle = "DBA Seniors";
+        job2._company = "Oracle";
+        job2._startYear = "2017";
+        job2._endYear = "2023";
+
+       //Console.WriteLine($"{job2._company} needs a {job2._jobTitle} in {job2._address}");
+        //job2.DisplayJobDetails();
+
+
+        Resume myResume = new Resume();
+
+        myResume._name = "Ernesto Quispe";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+
+        //Console.WriteLine($"{myResume._jobs[0]._jobTitle}");
+        
+        myResume.DisplayResume();
     }
 }
